fb43e1a7b3bfce536eb972e5ec659b122f8e1902
aniket01-ctrl <sakhareaniket81@gmail.com>
2025-11-18 22:17:13 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,83 @@
+import './style.css'
+
+import * as THREE from 'three';
+import spaceImg from './pexels-frank-cone-140140-3607542.jpg';
+import tesseract from './pexels-adrien-olichon-1257089-2387793.jpg';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+renderer.render(scene, camera);
+
+const geometry = new THREE.BoxGeometry(10, 10, 10);;
+const material = new THREE.MeshStandardMaterial({ color: 0x00fffb });
+const Box = new THREE.Mesh(geometry, material);
+
+scene.add(Box);
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5, 5, 5)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight)
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+scene.add(pointLight)
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  Box.rotation.x += 0.01;
+  Box.rotation.y += 0.005;
+  Box.rotation.z += 0.01;
+
+  Box2.rotation.x += 0.01;
+  Box2.rotation.y += 0.005;
+  Box2.rotation.z += 0.01;
+
+
+  controls.update();
+  renderer.render(scene, camera);
+
+}
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 16, 16);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star)
+
+}
+Array(200).fill().forEach(addStar)
+
+
+const spaceTexture = new THREE.TextureLoader().load(spaceImg);
+scene.background = spaceTexture;
+
+
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load(tesseract);
+
+const geometry2 = new THREE.BoxGeometry(10, 10, 10);
+const material2 = new THREE.MeshStandardMaterial({ map: texture });
+const Box2 = new THREE.Mesh(geometry2, material2);
+
+scene.add(Box2);
+
+animate()
